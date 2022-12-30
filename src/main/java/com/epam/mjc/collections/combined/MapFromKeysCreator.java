622143216b69d8a131cdb7edc794a2f67b40a9bc
Denis <106114126+DennisRoxxx@users.noreply.github.com>
2022-12-30 10:13:15 +0300

    Update MapFromKeysCreator.java

--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,25 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        
+        Map<Integer, Set<String>> map = new HashMap<>();
+        for (String string : sourceMap.keySet()) {
+            if (!map.containsKey(string.length())){
+                Set<String> tempString = new HashSet<>();
+                tempString.add(string);
+                map.put(string.length(),tempString);
+            }else {
+                map.get(string.length()).add(string);
+                map.put(string.length(), map.get(string.length()));
+            }
+        }
+
+        return map;
     }
 }

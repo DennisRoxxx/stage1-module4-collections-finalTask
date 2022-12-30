2446ecd66e60b9cd4df2e8423a62cf3e7b0ebf13
Denis <106114126+DennisRoxxx@users.noreply.github.com>
2022-12-30 10:22:24 +0300

    Update DeveloperProjectFinder.java

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -3,8 +3,27 @@ package com.epam.mjc.collections.combined;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.ArrayList;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+       List<String> list = new ArrayList<>();
+        for (Map.Entry<String, Set<String>> entry : projects.entrySet()) {
+            if (entry.getValue().contains(developer)) {
+                list.add(entry.getKey());
+            }
+        }
+        list.sort((
+            (o1, o2) -> {
+            int length_1 = o1.length();
+            int lenhth_2 = o2.length();
+            if (length_1 == lenhth_2) {
+                return o2.compareTo(o1);
+            }
+            return length_1 < lenhth_2 ? 1 : -1;
+        }
+        ));
+
+        return list;
     }
 }

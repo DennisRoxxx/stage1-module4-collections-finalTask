c849967c9c79b5777350cd9fe3bf5ae6b3026a5b
Denis <106114126+DennisRoxxx@users.noreply.github.com>
2022-12-30 10:05:55 +0300

    Update LessonsGetter.java

--- src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
+++ src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
@@ -1,10 +1,15 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class LessonsGetter {
     public Set<String> getLessons(Map<String, List<String>> timetable) {
+        TreeSet<String> lessons = new TreeSet<>();
+
+        for(List<String> days : timetable.values()){
+            lessons.addAll(days);
+        }
+
+        return lessons;
     }
 }
